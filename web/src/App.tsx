3ada8705f7a1d52443cbc414e54d1c154d3524d0
Ayush Bhagwat <ayushbhagwat921@gmail.com>
2026-09-04 18:06:26 +0530

    feat: scaffold React + Vite + TypeScript frontend
    Signed-off-by: Ayush Bhagwat <ayushbhagwat921@gmail.com>

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return <Header title="Freelance Marketplace" />;
+}
+
+function Header(props: { title: string }) {
+    return <h1>{props.title}</h1>;
+}
+
+export default App;
